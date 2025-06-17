42410f7d4e96f51d1e58a13b003b38835e528bb9
tqufkrrhmn <ktaufik498@gmail.com>
2025-06-17 15:55:14 +0700

    ini buat annas

--- app/Views/components/navbar.php
+++ app/Views/components/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar" role="navigation" aria-label="Main navigation">
         <ul class="nav-list">
-          <li><a href="#" class="nav-link" aria-current="page"><span class="material-icons nav-icon" aria-hidden="true">home</span> Beranda</a></li>
+          <li><a href="#*" class="nav-link" aria-current="page"><span class="material-icons nav-icon" aria-hidden="true">home</span> Beranda</a></li>
           <li class="nav-dropdown">
             <button aria-haspopup="true" aria-expanded="false" aria-controls="profile-menu" class="nav-button">
               <span class="material-icons nav-icon" aria-hidden="true">account_box</span> Profile
